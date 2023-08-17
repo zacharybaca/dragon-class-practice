77a0406b6a5d1420a621d2242a3bbb3037c3bf4b
zacharybaca <zachary.baca@msn.com>
2023-08-17 18:57:47 -0500

    completed phase 1

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,27 @@
 // Your code here
+class Dragon{
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
 
+  static getDragons(...arr) {
+    let dragonArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      dragonArray.push(arr[i].name)
+    }
+    return dragonArray;
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+// console.log(puff);
+// console.log(puff.breathesFire());
+// console.log(Dragon.getDragons(puff))
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
