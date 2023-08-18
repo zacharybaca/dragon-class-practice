9bf692a9ac23ad7b747eb161089f7e0a8627cb58
zacharybaca <zachary.baca@msn.com>
2023-08-18 15:56:23 -0500

    completed phase-2

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,24 @@
 // Your code here
+const Dragon = require("./dragon");
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals, friend) {
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        for (let i = 0; i < this.lifeGoals.length; i++) {
+            console.log(`${this.name} likes to ${this.lifeGoals[i]}`)
+        }
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`
+    }
+}
+
+
+
+module.exports = FriendlyDragon;
