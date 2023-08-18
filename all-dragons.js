b8c5363c1d7e8365989280cbcb3ab2f65c0c97b1
zacharybaca <zachary.baca@msn.com>
2023-08-18 16:14:07 -0500

    completed phase-4

--- all-dragons.js
+++ all-dragons.js
@@ -1,4 +1,12 @@
 // Your code here
+const Dragon = require("./classes/dragon");
+const FriendlyDragon = require("./classes/friendly-dragon");
+const EvilDragon = require("./classes/evil-dragon");
+
+let falkor = new FriendlyDragon("Falkor", "white", ["save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster"], "Bastian");
+let smaug = new EvilDragon("Smaug", "black", ["take over your mountain kingdom", "steal all your dwarven gold", "burn down your floating village"], "Dwarf King");
+
+let allDragons = Dragon.getDragons(falkor, smaug);
 
 
 /****************************************************************************/
@@ -7,6 +15,7 @@
 try {
   module.exports.falkor = falkor
   module.exports.smaug = smaug
+  module.exports.allDragons = allDragons
 } catch {
   module.exports = null;
 }
