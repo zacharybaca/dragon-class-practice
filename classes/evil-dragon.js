b99af1fb3cd4b5456eaaa8937f3d7943acb39141
zacharybaca <zachary.baca@msn.com>
2023-08-17 22:04:20 -0500

    completed phase 3

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,22 @@
 // Your code here
+const Dragon = require("./dragon");
+
+class EvilDragon extends Dragon{
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+    dontInviteThemOverForDinner() {
+        for (let i = 0; i < this.evilDoings.length; i++) {
+            console.log(`${this.name} will ${this.evilDoings[i]}`);
+        }
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+}
+
+
+module.exports = EvilDragon;
